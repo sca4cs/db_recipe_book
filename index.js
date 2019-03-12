f20b2939ff5f936d9a3e6b281aafa3a0c65caf17
sca4cs <shannon.atkinson.ca@gmail.com>
2019-03-12 18:33:35 -0400

    add home route

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ const db = require('./data/dbConfig.js');
 const server = express();
 server.use(express.json());
 
+server.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
 server.listen(8000, () => console.log('API running...'));
