b290579d1a13ca288ad0add5c51c6871ea5f128e
sca4cs <shannon.atkinson.ca@gmail.com>
2019-03-12 13:46:19 -0400

    set up server

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-//test comment 3
+const express = require('express');
+const db = require('./data/dbConfig.js');
+const server = express();
+server.use(express.json());
+
+server.listen(8000, () => console.log('API running...'));
